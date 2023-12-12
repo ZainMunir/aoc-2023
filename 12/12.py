57e2c1b9258133487072fcc20de2604a275e5f09
ZainMunir <zainmunir999@gmail.com>
2023-12-13 00:02:47 +0100

    update: optimisations for part 1, p2 too slow :(

--- 12/12.py
+++ 12/12.py
@@ -20,75 +20,114 @@ for line in sys.stdin:
     if line == "":
         continue
     springs, nums = line.split(" ")
-    springs = list(springs)
     nums = list(map(int, pattern.findall(nums)))
     all_springs.append(SpringLine(springs, nums))
 
 
 def check_line(springs, config):
-    groups = "".join(springs).split(".")
+    groups = springs.split(".")
     hash_counts = [group.count("#") for group in groups if group.count("#") > 0]
     return hash_counts == config
 
 
-def make_configs(springs, config, curr_spring, group_index=0, group_count=0):
+def make_configs(memo, springs, config, curr_spring, group_index=0, group_count=0):
+    if springs.count("#") > sum(config):
+        return 0
+    elif springs.count("#") + springs.count("?") < sum(config):
+        return 0
+    key = "".join(springs)
+    if key in memo:
+        return 0
     global iter
     iter += 1
+    if iter % 1000000 == 0:
+        print(iter)
 
     count = 0
     if "?" not in springs:
-        if "".join(springs) not in handled_combos:
-            handled_combos.add("".join(springs))
-            count = check_line(springs, config)
+        count = check_line(springs, config)
     else:
         for i in range(curr_spring, len(springs)):
             if springs[i] == "#":
-                count += make_configs(
-                    springs, config, i + 1, group_index, group_count + 1
-                )
+                group_count += 1
             elif springs[i] == ".":
                 if group_index == len(config):
-                    count += make_configs(springs, config, i + 1, group_index)
-                elif group_count == config[group_index]:
-                    count += make_configs(springs, config, i + 1, group_index + 1)
-                elif group_count != 0:
-                    count += make_configs(springs, config, i + 1, group_index + 1)
+                    group_count = 0
+                elif group_count == config[group_index] or group_count != 0:
+                    group_count = 0
+                    group_index += 1
             elif springs[i] == "?":
                 if group_index == len(config):
-                    springs[i] = "."
-                    count += make_configs(springs, config, i + 1, group_index)
-                    springs[i] = "?"
+                    count += make_configs(
+                        memo,
+                        springs[:i] + "." + springs[i + 1 :],
+                        config,
+                        i + 1,
+                        group_index,
+                    )
                 elif group_count == config[group_index]:
-                    springs[i] = "."
-                    count += make_configs(springs, config, i + 1, group_index + 1)
-                    springs[i] = "?"
+                    count += make_configs(
+                        memo,
+                        springs[:i] + "." + springs[i + 1 :],
+                        config,
+                        i + 1,
+                        group_index + 1,
+                    )
                 elif group_count != 0:
-                    springs[i] = "#"
                     count += make_configs(
-                        springs, config, i + 1, group_index, group_count + 1
+                        memo,
+                        springs[:i] + "#" + springs[i + 1 :],
+                        config,
+                        i + 1,
+                        group_index,
+                        group_count + 1,
                     )
-                    springs[i] = "."
-                    count += make_configs(springs, config, i + 1, group_index + 1)
-                    springs[i] = "?"
+                    count += make_configs(
+                        memo,
+                        springs[:i] + "." + springs[i + 1 :],
+                        config,
+                        i + 1,
+                        group_index + 1,
+                    )
                 else:
-                    springs[i] = "#"
                     count += make_configs(
-                        springs, config, i + 1, group_index, group_count + 1
+                        memo,
+                        springs[:i] + "#" + springs[i + 1 :],
+                        config,
+                        i + 1,
+                        group_index,
+                        group_count + 1,
                     )
-                    springs[i] = "."
-                    count += make_configs(springs, config, i + 1, group_index)
-                    springs[i] = "?"
+                    count += make_configs(
+                        memo,
+                        springs[:i] + "." + springs[i + 1 :],
+                        config,
+                        i + 1,
+                        group_index,
+                    )
                 break
+    memo[key] = count
     return count
 
 
 total = 0
 for spring in all_springs:
-    handled_combos = set()
     iter = 0
     print(math.pow(4, spring.springs.count("?")))
-    handled_combos = set()
-    val = make_configs(spring.springs, spring.config, 0)
+    val = make_configs(dict(), spring.springs, spring.config, 0)
     total += val
-    print(val, total, spring.config)
+    print(val, total, iter, spring.config)
 print(total)
+
+
+# total = 0
+# for spring in all_springs:
+#     iter = 0
+#     spring.springs = "?".join([spring.springs] * 5)
+#     spring.config = spring.config * 5
+#     memo = dict()
+#     print("".join(spring.springs), spring.config)
+#     val = make_configs(memo, spring.springs, spring.config, 0)
+#     total += val
+#     print(val, total, spring.config)
+# print(total)
